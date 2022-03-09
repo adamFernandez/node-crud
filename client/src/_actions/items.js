251600b80e2bb926d00554ca8375e6ea13d103fb
Alesito <aarodriguezfernandez@gmail.com>
2022-03-09 17:16:50 +0000

    Created api,actions,reducers at the client side

---
+++ client/src/_actions/items.js
@@ -0,0 +1,21 @@
+import * as api from '../api/index';
+
+export const createItem = (item) => async (dispatch) => {
+  const { data } = await api.createItem(item);
+  dispatch({ type: 'POST', payload: data });
+}
+
+export const getItems = (item) => async (dispatch) => {
+  const { data } = await api.getItems();
+  dispatch({ type: 'GET', payload: data });
+}
+
+export const updateItem = (id, item) => async (dispatch) => {
+  const { data } = await api.updateItem(id, item);
+  dispatch({ type: 'UPDATE', payload: data });
+}
+
+export const deleteItem = (id) => async (dispatch) => {
+  const { data } = await api.deleteItem(id);
+  dispatch({ type: 'DELETE', payload: data });
+}
